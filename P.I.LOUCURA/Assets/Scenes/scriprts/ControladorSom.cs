e05982e6d018c0d5585bbcd19446ada22ae68958
Arthur da Cruz <cruzz16.mr@gmail.com>
2024-08-21 15:47:17 -0300

    add som

--- P.I.LOUCURA/Assets/Scenes/scriprts/ControladorSom.cs
+++ P.I.LOUCURA/Assets/Scenes/scriprts/ControladorSom.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControladorSom : MonoBehaviour
 {
-    public void LigarDesligarSom;
+    public bool estadoSom = true;
+    [SerializeField] private AudioSource fundoMusical;
+
+    [SerializeField] private Sprite somLigadoSprite;
+    [SerializeField] private Sprite somDesligadoSprite;
+
+    [SerializeField] private Image muteImage;
+    public void LigarDesligarSom()
+    {
+        estadoSom = !estadoSom;
+        fundoMusical.enabled = estadoSom;
+
+        if(estadoSom) 
+        { 
+        muteImage.sprite = somLigadoSprite;
+        }
+        else
+        {
+            muteImage.sprite = somDesligadoSprite;
+        }
+    }
+    public void VolumeMusical(float value)
+    {
+    fundoMusical.volume = value;
+    }
 }
